0de3a1e33702b5f0abe14eeb0bcdec157170ed8f
yacine bouklif <y.bouklif@it-students.fr>
2024-10-24 11:56:59 +0200

    Ajout fichier

---
+++ D71-12FactorApps/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Base route
+app.get('/', (req, res) => {
+    res.send('FactorApp API is running');
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
